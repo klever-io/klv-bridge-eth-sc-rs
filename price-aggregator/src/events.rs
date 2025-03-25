32a23c14b4d8b063770a0b484eac6928f5edd1c8
Romulo Siebra <romulosiebra147@gmail.com>
2025-03-25 17:35:54 -0300

    add price-aggregator contract and staking module

---
+++ price-aggregator/src/events.rs
@@ -0,0 +1,106 @@
+klever_sc::imports!();
+klever_sc::derive_imports!();
+
+use crate::price_aggregator_data::{TimestampedPrice, TokenPair};
+pub type RoundId = u32;
+pub type Round = usize;
+pub type Block = u64;
+pub type Epoch = u64;
+pub type Timestamp = u64;
+
+#[type_abi]
+#[derive(TopEncode)]
+pub struct NewRoundEvent<M: ManagedTypeApi> {
+    price: BigUint<M>,
+    timestamp: Timestamp,
+    decimals: u8,
+    block: Block,
+    epoch: Epoch,
+}
+
+#[type_abi]
+#[derive(TopEncode)]
+pub struct DiscardSubmissionEvent {
+    submission_timestamp: Timestamp,
+    first_submission_timestamp: Timestamp,
+    has_caller_already_submitted: bool,
+}
+
+#[klever_sc::module]
+pub trait EventsModule {
+    fn emit_new_round_event(
+        &self,
+        token_pair: &TokenPair<Self::Api>,
+        round: Round,
+        price_feed: &TimestampedPrice<Self::Api>,
+    ) {
+        let epoch = self.blockchain().get_block_epoch();
+        self.new_round_event(
+            &token_pair.from.clone(),
+            &token_pair.to.clone(),
+            round,
+            &NewRoundEvent {
+                price: price_feed.price.clone(),
+                timestamp: price_feed.timestamp,
+                decimals: price_feed.decimals,
+                block: self.blockchain().get_block_nonce(),
+                epoch,
+            },
+        )
+    }
+
+    #[event("new_round")]
+    fn new_round_event(
+        &self,
+        #[indexed] from: &ManagedBuffer,
+        #[indexed] to: &ManagedBuffer,
+        #[indexed] round: Round,
+        new_round_event: &NewRoundEvent<Self::Api>,
+    );
+
+    fn emit_discard_submission_event(
+        &self,
+        token_pair: &TokenPair<Self::Api>,
+        round: Round,
+        submission_timestamp: Timestamp,
+        first_submission_timestamp: Timestamp,
+        has_caller_already_submitted: bool,
+    ) {
+        self.discard_submission_event(
+            &token_pair.from.clone(),
+            &token_pair.to.clone(),
+            round,
+            &DiscardSubmissionEvent {
+                submission_timestamp,
+                first_submission_timestamp,
+                has_caller_already_submitted,
+            },
+        )
+    }
+
+    #[event("discard_submission")]
+    fn discard_submission_event(
+        &self,
+        #[indexed] from: &ManagedBuffer,
+        #[indexed] to: &ManagedBuffer,
+        #[indexed] round: Round,
+        discard_submission_event: &DiscardSubmissionEvent,
+    );
+
+    #[event("discard_round")]
+    fn discard_round_event(
+        &self,
+        #[indexed] from: &ManagedBuffer,
+        #[indexed] to: &ManagedBuffer,
+        #[indexed] round: Round,
+    );
+
+    #[event("add_submission")]
+    fn add_submission_event(
+        &self,
+        #[indexed] from: &ManagedBuffer,
+        #[indexed] to: &ManagedBuffer,
+        #[indexed] round: Round,
+        price: &BigUint,
+    );
+}
---
+++ price-aggregator/tests/price_aggregator_whitebox_test.rs
@@ -0,0 +1,494 @@
+use klv_price_aggregator_sc::{
+    price_aggregator_data::{OracleStatus, TimestampedPrice, TokenPair},
+    PriceAggregator, MAX_ROUND_DURATION_SECONDS,
+};
+use klever_sc_modules::{pause::PauseModule, staking::StakingModule};
+use klever_sc_scenario::imports::*;
+
+pub const DECIMALS: u8 = 0;
+pub const EGLD_TICKER: &[u8] = b"EGLD";
+pub const NR_ORACLES: usize = 4;
+pub const SLASH_AMOUNT: u64 = 10;
+pub const SLASH_QUORUM: usize = 3;
+pub const STAKE_AMOUNT: u64 = 20;
+pub const SUBMISSION_COUNT: usize = 3;
+pub const USD_TICKER: &[u8] = b"USDC";
+
+const OWNER_ADDRESS: TestAddress = TestAddress::new("owner");
+const PRICE_AGGREGATOR_ADDRESS: TestSCAddress = TestSCAddress::new("price-aggregator");
+const PRICE_AGGREGATOR_PATH_EXPR: KleverscPath =
+    KleverscPath::new("kleversc:output/klv-price-aggregator-sc.mxsc.json");
+
+fn world() -> ScenarioWorld {
+    let mut blockchain = ScenarioWorld::new();
+    
+    blockchain.set_current_dir_from_workspace("contracts/core/price-aggregator");
+    blockchain.register_contract(
+        PRICE_AGGREGATOR_PATH_EXPR,
+        klv_price_aggregator_sc::ContractBuilder,
+    );
+
+    blockchain
+}
+
+#[test]
+fn test_price_aggregator_submit() {
+    let (mut world, oracles) = setup();
+
+    // configure the number of decimals
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.set_pair_decimals(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                DECIMALS,
+            )
+        });
+
+    // try submit while paused
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .returns(ExpectError(4u64, "Contract is paused"))
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                99,
+                managed_biguint!(100),
+                DECIMALS,
+            )
+        });
+
+    // unpause
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.unpause_endpoint();
+        });
+
+    // submit first timestamp too old
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .returns(ExpectError(4u64, "First submission too old"))
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                10,
+                managed_biguint!(100),
+                DECIMALS,
+            )
+        });
+
+    // submit ok
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(100),
+                DECIMALS,
+            )
+        });
+
+    let current_timestamp = 100;
+    world.query().to(PRICE_AGGREGATOR_ADDRESS).whitebox(
+        klv_price_aggregator_sc::contract_obj,
+        |sc| {
+            let token_pair = TokenPair {
+                from: managed_buffer!(EGLD_TICKER),
+                to: managed_buffer!(USD_TICKER),
+            };
+            assert_eq!(
+                sc.first_submission_timestamp(&token_pair).get(),
+                current_timestamp
+            );
+            assert_eq!(
+                sc.last_submission_timestamp(&token_pair).get(),
+                current_timestamp
+            );
+
+            let submissions = sc.submissions().get(&token_pair).unwrap();
+            assert_eq!(submissions.len(), 1);
+            assert_eq!(
+                submissions.get(&ManagedAddress::from(&oracles[0])).unwrap(),
+                managed_biguint!(100)
+            );
+
+            assert_eq!(
+                sc.oracle_status()
+                    .get(&ManagedAddress::from(&oracles[0]))
+                    .unwrap(),
+                OracleStatus {
+                    total_submissions: 1,
+                    accepted_submissions: 1
+                }
+            );
+        },
+    );
+
+    // first oracle submit again - submission not accepted
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(100),
+                DECIMALS,
+            )
+        });
+
+    world.query().to(PRICE_AGGREGATOR_ADDRESS).whitebox(
+        klv_price_aggregator_sc::contract_obj,
+        |sc| {
+            assert_eq!(
+                sc.oracle_status()
+                    .get(&ManagedAddress::from(&oracles[0]))
+                    .unwrap(),
+                OracleStatus {
+                    total_submissions: 2,
+                    accepted_submissions: 1
+                }
+            );
+        },
+    );
+}
+
+#[test]
+fn test_price_aggregator_submit_round_ok() {
+    let (mut world, oracles) = setup();
+
+    // configure the number of decimals
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.set_pair_decimals(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                DECIMALS,
+            )
+        });
+
+    // unpause
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.unpause_endpoint();
+        });
+
+    // submit first
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(10_000),
+                DECIMALS,
+            )
+        });
+
+    let current_timestamp = 110;
+    world.current_block().block_timestamp(current_timestamp);
+
+    // submit second
+    world
+        .tx()
+        .from(&oracles[1])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                101,
+                managed_biguint!(11_000),
+                DECIMALS,
+            )
+        });
+
+    // submit third
+    world
+        .tx()
+        .from(&oracles[2])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                105,
+                managed_biguint!(12_000),
+                DECIMALS,
+            )
+        });
+
+    world.query().to(PRICE_AGGREGATOR_ADDRESS).whitebox(
+        klv_price_aggregator_sc::contract_obj,
+        |sc| {
+            let result =
+                sc.latest_price_feed(managed_buffer!(EGLD_TICKER), managed_buffer!(USD_TICKER));
+
+            let (round_id, from, to, timestamp, price, decimals) = result.into_tuple();
+            assert_eq!(round_id, 1);
+            assert_eq!(from, managed_buffer!(EGLD_TICKER));
+            assert_eq!(to, managed_buffer!(USD_TICKER));
+            assert_eq!(timestamp, current_timestamp);
+            assert_eq!(price, managed_biguint!(11_000));
+            assert_eq!(decimals, DECIMALS);
+
+            // submissions are deleted after round is created
+            let token_pair = TokenPair { from, to };
+            let submissions = sc.submissions().get(&token_pair).unwrap();
+            assert_eq!(submissions.len(), 0);
+
+            let rounds = sc.rounds().get(&token_pair).unwrap();
+            assert_eq!(rounds.len(), 1);
+            assert_eq!(
+                rounds.get(1),
+                TimestampedPrice {
+                    timestamp,
+                    price,
+                    decimals
+                }
+            );
+        },
+    );
+}
+
+#[test]
+fn test_price_aggregator_discarded_round() {
+    let (mut world, oracles) = setup();
+
+    // configure the number of decimals
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.set_pair_decimals(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                DECIMALS,
+            )
+        });
+
+    // unpause
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.unpause_endpoint();
+        });
+
+    // submit first
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(10_000),
+                DECIMALS,
+            )
+        });
+
+    let current_timestamp = 100 + MAX_ROUND_DURATION_SECONDS + 1;
+    world.current_block().block_timestamp(current_timestamp);
+
+    // submit second - this will discard the previous submission
+    world
+        .tx()
+        .from(&oracles[1])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                current_timestamp - 1,
+                managed_biguint!(11_000),
+                DECIMALS,
+            )
+        });
+
+    world.query().to(PRICE_AGGREGATOR_ADDRESS).whitebox(
+        klv_price_aggregator_sc::contract_obj,
+        |sc| {
+            let token_pair = TokenPair {
+                from: managed_buffer!(EGLD_TICKER),
+                to: managed_buffer!(USD_TICKER),
+            };
+            let submissions = sc.submissions().get(&token_pair).unwrap();
+            assert_eq!(submissions.len(), 1);
+            assert_eq!(
+                submissions.get(&managed_address!(&oracles[1])).unwrap(),
+                managed_biguint!(11_000)
+            );
+        },
+    );
+}
+
+#[test]
+fn test_price_aggregator_slashing() {
+    let (mut world, oracles) = setup();
+
+    // configure the number of decimals
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.set_pair_decimals(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                DECIMALS,
+            )
+        });
+
+    // unpause
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.unpause_endpoint();
+        });
+
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.vote_slash_member(ManagedAddress::from(&oracles[1]));
+        });
+
+    world
+        .tx()
+        .from(&oracles[2])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.vote_slash_member(ManagedAddress::from(&oracles[1]))
+        });
+
+    world
+        .tx()
+        .from(&oracles[3])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.vote_slash_member(ManagedAddress::from(&oracles[1]));
+        });
+
+    world.query().to(PRICE_AGGREGATOR_ADDRESS).whitebox(
+        klv_price_aggregator_sc::contract_obj,
+        |sc| {
+            let list = sc.slashing_proposal_voters(&ManagedAddress::from(&oracles[1]));
+            assert!(list.contains(&ManagedAddress::from(&oracles[0])));
+            assert!(list.contains(&ManagedAddress::from(&oracles[2])));
+            assert!(list.contains(&ManagedAddress::from(&oracles[3])));
+        },
+    );
+
+    world
+        .tx()
+        .from(&oracles[0])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.slash_member(ManagedAddress::from(&oracles[1]));
+        });
+
+    // oracle 1 try submit after slashing
+    world
+        .tx()
+        .from(&oracles[1])
+        .to(PRICE_AGGREGATOR_ADDRESS)
+        .returns(ExpectError(4u64, "only oracles allowed"))
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(10_000),
+                DECIMALS,
+            )
+        });
+}
+
+fn setup() -> (ScenarioWorld, Vec<Address>) {
+    // setup
+    let mut world = world();
+
+    world.account(OWNER_ADDRESS).nonce(1);
+    world.current_block().block_timestamp(100);
+
+    let mut oracles = Vec::new();
+    for i in 1..=NR_ORACLES {
+        let oracle_address_expr = format!("oracle{i}");
+        let oracle_address = TestAddress::new(&oracle_address_expr);
+
+        world.account(oracle_address).nonce(1).balance(STAKE_AMOUNT);
+
+        oracles.push(oracle_address.to_address());
+    }
+
+    // init price aggregator
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .raw_deploy()
+        .code(PRICE_AGGREGATOR_PATH_EXPR)
+        .new_address(PRICE_AGGREGATOR_ADDRESS)
+        .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+            let mut oracle_args = MultiValueEncoded::new();
+            for oracle_address in &oracles {
+                oracle_args.push(ManagedAddress::from(oracle_address));
+            }
+
+            sc.init(
+                EgldOrEsdtTokenIdentifier::egld(),
+                managed_biguint!(STAKE_AMOUNT),
+                managed_biguint!(SLASH_AMOUNT),
+                SLASH_QUORUM,
+                SUBMISSION_COUNT,
+                oracle_args,
+            )
+        });
+
+    for oracle_address in &oracles {
+        world
+            .tx()
+            .from(oracle_address)
+            .to(PRICE_AGGREGATOR_ADDRESS)
+            .egld(STAKE_AMOUNT)
+            .whitebox(klv_price_aggregator_sc::contract_obj, |sc| {
+                sc.stake();
+            });
+    }
+
+    (world, oracles)
+}

096061d974166f2e193e43048e04f8e83d0a6203
Romulo Siebra <romulosiebra147@gmail.com>
2025-05-15 18:32:13 -0300

    change multitransfer-snippets to klever operator

--- multisig/interaction/config/multitransfer-snippets.sh
+++ multisig/interaction/config/multitransfer-snippets.sh
@@ -1,55 +1,51 @@
 deployMultiTransfer() {
     CHECK_VARIABLES MULTI_TRANSFER_WASM
 
-    mxpy contract deploy --bytecode=${MULTI_TRANSFER_WASM} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=100000000 --metadata-payable \
-    --send --outfile="deploy-multitransfer-testnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
+    SC_RESULT=$(eval operator sc create --key-file=${ALICE} --wasm ${MULTI_TRANSFER_WASM} \
+    --await --result-only --sign --node ${PROXY})
 
-    ADDRESS=$(mxpy data parse --file="./deploy-multitransfer-testnet.interaction.json" --expression="data['contractAddress']")
-    mxpy data store --key=address-testnet-multitransfer --value=${ADDRESS}
+    check_result ${SC_RESULT}
+
+    CONTRACT_ADDRESS=$(jq '.logs.events[] | select(.identifier=="SCDeploy") | .address' <<< "${SC_RESULT}")
+    CONTRACT_ADDRESS=$(echo ${CONTRACT_ADDRESS} | tr -d '"')
 
     echo ""
-    echo "Multi transfer contract address: ${ADDRESS}"
-    update-config MULTI_TRANSFER ${ADDRESS}
-}
-
-setBridgeProxyContractAddressOnMultiTransfer() {
-    CHECK_VARIABLES MULTI_TRANSFER BRIDGE_PROXY
-
-    mxpy contract call ${MULTI_TRANSFER} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=60000000 --function="setBridgeProxyContractAddress" \
-    --arguments ${BRIDGE_PROXY} \
-    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    echo "Multi transfer contract address: ${CONTRACT_ADDRESS}"
+    update-config MULTI_TRANSFER ${CONTRACT_ADDRESS}
 }
 
 setBridgedTokensWrapperOnMultiTransfer() {
     CHECK_VARIABLES MULTI_TRANSFER BRIDGED_TOKENS_WRAPPER
 
-    mxpy contract call ${MULTI_TRANSFER} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=60000000 --function="setWrappingContractAddress" \
-    --arguments ${BRIDGED_TOKENS_WRAPPER} \
-    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    operator sc invoke ${MULTI_TRANSFER} setWrappingContractAddress --key-file=${ALICE} \
+    --args A:${BRIDGED_TOKENS_WRAPPER} \
+    --await --sign --node ${PROXY}
 }
 
 deployMultiTransferForUpgrade() {
     CHECK_VARIABLES MULTI_TRANSFER_WASM
 
-    mxpy contract deploy --bytecode=${MULTI_TRANSFER_WASM} --recall-nonce "${MXPY_SIGN[@]}" \
-        --gas-limit=100000000 --metadata-payable \
-        --send --outfile="deploy-multitransfer-upgrade.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
+    SC_RESULT=$(eval operator sc create --key-file=${ALICE} --wasm ${MULTI_TRANSFER_WASM} \
+    --await --result-only --sign --node ${PROXY})
 
-    TRANSACTION=$(mxpy data parse --file="./deploy-multitransfer-upgrade.interaction.json" --expression="data['emittedTransactionHash']")
-    ADDRESS=$(mxpy data parse --file="./deploy-multitransfer-upgrade.interaction.json" --expression="data['contractAddress']")
+    check_result ${SC_RESULT}
+
+    CONTRACT_ADDRESS=$(jq '.logs.events[] | select(.identifier=="SCDeploy") | .address' <<< "${SC_RESULT}")
+    CONTRACT_ADDRESS=$(echo ${CONTRACT_ADDRESS} | tr -d '"')
 
     echo ""
-    echo "New multi transfer contract address: ${ADDRESS}"
+    echo "New multi transfer contract address: ${CONTRACT_ADDRESS}"
+    
+    # Store for later use in upgradeMultiTransferContract
+    NEW_MULTI_TRANSFER_ADDR=${CONTRACT_ADDRESS}
 }
 
 upgradeMultiTransferContract() {
-    local NEW_MULTI_TRANSFER_ADDR=$(mxpy data parse --file="./deploy-multitransfer-upgrade.interaction.json" --expression="data['contractAddress']")
+    CHECK_VARIABLES MULTISIG MULTI_TRANSFER NEW_MULTI_TRANSFER_ADDR
 
-    mxpy contract call ${MULTISIG} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=400000000 --function="upgradeChildContractFromSource" \
-    --arguments ${MULTI_TRANSFER} ${NEW_MULTI_TRANSFER_ADDR} 0x00 \
-    --send --outfile="upgrade-multitransfer-child-sc.json" --proxy=${PROXY} --chain=${CHAIN_ID}
+    operator sc invoke ${MULTISIG} upgradeChildContractFromSource --key-file=${ALICE} \
+    --args A:${MULTI_TRANSFER} --args A:${NEW_MULTI_TRANSFER_ADDR} --args bool:false \
+    --await --sign --node ${PROXY}
+
+    update-config MULTI_TRANSFER ${NEW_MULTI_TRANSFER_ADDR}
 }

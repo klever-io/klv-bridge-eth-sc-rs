8f99a29eb9cba92261002cbb221def69fa820166
Romulo Siebra <romulosiebra147@gmail.com>
2025-05-21 08:05:43 -0300

    change proxy scripts to use operator cli

--- multisig/interaction/config/proxy-snippets.sh
+++ multisig/interaction/config/proxy-snippets.sh
@@ -1,69 +1,69 @@
 deployBridgeProxy() {
     CHECK_VARIABLES PROXY_WASM MULTI_TRANSFER
 
-    mxpy contract deploy --bytecode=${PROXY_WASM} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=200000000 \
-    --arguments ${MULTI_TRANSFER} \
-    --send --outfile="deploy-proxy-testnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
+    SC_RESULT=$(eval operator sc create --key-file=${ALICE} --wasm ${PROXY_WASM} \
+    --args A:${MULTI_TRANSFER} \
+    --await --result-only --sign --node ${PROXY})
 
-    TRANSACTION=$(mxpy data parse --file="./deploy-proxy-testnet.interaction.json" --expression="data['emitted_tx']['hash']")
-    ADDRESS=$(mxpy data parse --file="./deploy-proxy-testnet.interaction.json" --expression="data['contractAddress']")
+    check_result ${SC_RESULT}
 
-    # mxpy data store --key=address-testnet-proxy --value=${ADDRESS}
-    # mxpy data store --key=deployTransaction-testnet --value=${TRANSACTION}
+    CONTRACT_ADDRESS=$(jq '.logs.events[] | select(.identifier=="SCDeploy") | .address' <<< "${SC_RESULT}")
+    CONTRACT_ADDRESS=$(echo ${CONTRACT_ADDRESS} | tr -d '"')
 
     echo ""
-    echo "Proxy contract address: ${ADDRESS}"
-    update-config BRIDGE_PROXY ${ADDRESS}
+    echo "Proxy contract address: ${CONTRACT_ADDRESS}"
+    update-config BRIDGE_PROXY ${CONTRACT_ADDRESS}
 }
 
 setBridgedTokensWrapperOnSCProxy() {
     CHECK_VARIABLES BRIDGE_PROXY BRIDGED_TOKENS_WRAPPER
 
-    mxpy contract call ${BRIDGE_PROXY} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=60000000 --function="setBridgedTokensWrapperAddress" \
-    --arguments ${BRIDGED_TOKENS_WRAPPER} \
-    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    operator sc invoke ${BRIDGE_PROXY} setBridgedTokensWrapperAddress --key-file=${ALICE} \
+    --args A:${BRIDGED_TOKENS_WRAPPER} \
+    --await --sign --node ${PROXY}
 }
 
 setMultiTransferOnSCProxy() {
     CHECK_VARIABLES BRIDGE_PROXY MULTI_TRANSFER
 
-    mxpy contract call ${BRIDGE_PROXY} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=60000000 --function="setMultiTransferAddress" \
-    --arguments ${MULTI_TRANSFER} \
-    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    operator sc invoke ${BRIDGE_PROXY} setMultiTransferAddress --key-file=${ALICE} \
+    --args A:${MULTI_TRANSFER} \
+    --await --sign --node ${PROXY}
 }
 
-setEsdtSafeOnSCProxy() {
+setKdaSafeOnSCProxy() {
     CHECK_VARIABLES BRIDGE_PROXY SAFE
 
-    mxpy contract call ${BRIDGE_PROXY} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=60000000 --function="setEsdtSafeAddress" \
-    --arguments ${SAFE} \
-    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    operator sc invoke ${BRIDGE_PROXY} setKdaSafeAddress --key-file=${ALICE} \
+    --args A:${SAFE} \
+    --await --sign --node ${PROXY}
 }
 
 deployBridgeProxyForUpgrade() {
     CHECK_VARIABLES PROXY_WASM MULTI_TRANSFER
 
-    mxpy contract deploy --bytecode=${PROXY_WASM} --recall-nonce "${MXPY_SIGN[@]}" \
-        --gas-limit=200000000 \
-        --arguments ${MULTI_TRANSFER} \
-        --send --outfile="deploy-proxy-upgrade.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
+    SC_RESULT=$(eval operator sc create --key-file=${ALICE} --wasm ${PROXY_WASM} \
+    --args A:${MULTI_TRANSFER} \
+    --await --result-only --sign --node ${PROXY})
 
-    TRANSACTION=$(mxpy data parse --file="./deploy-proxy-upgrade.interaction.json" --expression="data['emittedTransactionHash']")
-    ADDRESS=$(mxpy data parse --file="./deploy-proxy-upgrade.interaction.json" --expression="data['contractAddress']")
+    check_result ${SC_RESULT}
+
+    CONTRACT_ADDRESS=$(jq '.logs.events[] | select(.identifier=="SCDeploy") | .address' <<< "${SC_RESULT}")
+    CONTRACT_ADDRESS=$(echo ${CONTRACT_ADDRESS} | tr -d '"')
 
     echo ""
-    echo "New proxy contract address: ${ADDRESS}"
+    echo "New proxy contract address: ${CONTRACT_ADDRESS}"
+    
+    # Store for later use in upgradeBridgeProxyContract
+    NEW_BRIDGE_PROXY_ADDR=${CONTRACT_ADDRESS}
 }
 
 upgradeBridgeProxyContract() {
-    local NEW_BRIDGE_PROXY_ADDR=$(mxpy data parse --file="./deploy-proxy-upgrade.interaction.json" --expression="data['contractAddress']")
+    CHECK_VARIABLES MULTISIG BRIDGE_PROXY NEW_BRIDGE_PROXY_ADDR
 
-    mxpy contract call ${MULTISIG} --recall-nonce "${MXPY_SIGN[@]}" \
-    --gas-limit=400000000 --function="upgradeChildContractFromSource" \
-    --arguments ${BRIDGE_PROXY} ${NEW_BRIDGE_PROXY_ADDR} 0x00 \
-    --send --outfile="upgrade-proxy-child-sc.json" --proxy=${PROXY} --chain=${CHAIN_ID}
+    operator sc invoke ${MULTISIG} upgradeChildContractFromSource --key-file=${ALICE} \
+    --args A:${BRIDGE_PROXY} --args A:${NEW_BRIDGE_PROXY_ADDR} --args bool:false \
+    --await --sign --node ${PROXY}
+
+    update-config BRIDGE_PROXY ${NEW_BRIDGE_PROXY_ADDR}
 }

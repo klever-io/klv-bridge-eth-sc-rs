1067c68b8b59dc1466832e2c61c3e3ff75825314
Romulo Siebra <romulosiebra147@gmail.com>
2025-07-15 11:06:47 -0300

    add timestamp correction for transaction simulation

--- price-aggregator/src/lib.rs
+++ price-aggregator/src/lib.rs
@@ -16,6 +16,8 @@ pub const MAX_ROUND_DURATION_SECONDS: u64 = 1_800; // 30 minutes
 const PAUSED_ERROR_MSG: &[u8] = b"Contract is paused";
 const PAIR_DECIMALS_NOT_CONFIGURED_ERROR: &[u8] = b"pair decimals not configured";
 const WRONG_NUMBER_OF_DECIMALS_ERROR: &[u8] = b"wrong number of decimals";
+// Correction for transaction simulation, to avoid issues with block timestamp
+const TIME_SIMULATE_BLOCK_CORRECTION: u64 = 5; // seconds
 
 #[klever_sc::contract]
 pub trait PriceAggregator:
@@ -216,7 +218,7 @@ pub trait PriceAggregator:
     }
 
     fn require_valid_submission_timestamp(&self, submission_timestamp: u64) {
-        let current_timestamp = self.blockchain().get_block_timestamp();
+        let current_timestamp = self.blockchain().get_block_timestamp() + TIME_SIMULATE_BLOCK_CORRECTION;
         require!(
             submission_timestamp <= current_timestamp,
             "Timestamp is from the future"

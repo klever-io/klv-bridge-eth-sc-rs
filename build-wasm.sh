90c212c83212dc5a71c9664c607708940920662f
Romulo Siebra <romulosiebra147@gmail.com>
2025-11-25 07:55:49 -0300

    chore: update build scripts

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-# builds all wasm targets
+# builds all wasm targets and generates proxies
 
 set -e
-SMART_CONTRACT_JSONS=$(find . -name "elrond.json")
-for smart_contract_json in $SMART_CONTRACT_JSONS
+
+KSC=~/klever-sdk/ksc
+
+# Find all contract directories (those containing meta/ folder with Cargo.toml)
+CONTRACTS=$(find . -type d -name "meta" -exec dirname {} \; | grep -v target | sort -u)
+
+for contract_path in $CONTRACTS
 do
-    smart_contract_folder=$(dirname $smart_contract_json)
     echo ""
-    (set -x; mxpy --verbose contract build $smart_contract_folder --ignore-eei-checks)
+    echo "Building contract: $contract_path"
+    (set -x; $KSC all build --path "$contract_path")
+    
+    echo ""
+    echo "Generating proxies: $contract_path"
+    (set -x; $KSC all proxy --path "$contract_path")
 done
+
+echo ""
+echo "All contracts built and proxies generated successfully!"
